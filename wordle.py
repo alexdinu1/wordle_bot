57904a158afe89f3b796cbebfce02c524a3b76ae
alexdinu1 <alexdinu6@gmail.com>
2024-09-23 10:03:39 +0800

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -5,7 +5,6 @@ letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','
 
 start = "naieo"
 
-'''
 def prob(sample):
     zero = []
 
@@ -15,17 +14,20 @@ def prob(sample):
     
     return len(zero)/len(words)
 
-prob_list = {}
+def next_word():
+    prob_list = {}
 
-for word in words:
-    prob_list[word] = prob(word)
+    for word in words:
+        prob_list[word] = prob(word)
+        
+    lowest = min(prob_list, key=prob_list.get)
     
-lowest = min(prob_list, key=prob_list.get)
+    return lowest
 
-'''
+
 
 def play():
-    word = "naieo"
+    word = "naieo"  # Replace with the guessed word for each round
     print(word)
     
     result = None
@@ -33,26 +35,56 @@ def play():
     while result != "ggggg":
         result = input("please input the result (b for gray, y for yellow, g for green)\n")
         
-        pairs = []
-        glist = []
+        # Keep track of letter counts for handling multiple occurrences
+        correct_positions = {}  # Tracks 'g' positions
+        yellow_positions = {}  # Tracks 'y' positions
         
-        filtered_words = words[:]  # Create a copy of the list to avoid modifying it directly
-        for n, (i, j) in enumerate(zip(result, word)):
-            pairs.append((i,j,n))
+        # First pass to handle green ('g') and yellow ('y') letters
+        for i in range(len(result)):
+            if result[i] == 'g':
+                correct_positions[word[i]] = correct_positions.get(word[i], 0) + 1
+            elif result[i] == 'y':
+                yellow_positions[word[i]] = yellow_positions.get(word[i], 0) + 1
         
-        for pair in pairs:
-            if pair[0] == "g":
-                for w in words:
-                    if w[n] == pair[1]:
-                        print(w)
-                        words.remove(w)
+        for k in words[:]:  # Use a copy of the list to avoid modifying it while iterating
+            remove_word = False  # Flag to track if the word should be removed
             
-           
-        word = words[0]
-        print("this is the word!!!! ", word)
-        words.remove(word)
-    
-        print(glist)
-    return word
+            for i in range(len(result)):
+                if result[i] == "g":
+                    if k[i] != word[i]:
+                        remove_word = True
+                
+                elif result[i] == "y":
+                    # Check that the letter exists but not in the same position
+                    if k[i] == word[i] or word[i] not in k:
+                        remove_word = True
+                
+                elif result[i] == "b":
+                    # Count the occurrences of the gray letter in the current word (guess)
+                    if word[i] in correct_positions:
+                        # If 'g' or 'y' was already marked for this letter, handle carefully
+                        if k.count(word[i]) > correct_positions[word[i]] + yellow_positions.get(word[i], 0):
+                            remove_word = True
+                    else:
+                        # If no 'g' or 'y', then the gray letter shouldn't appear in the word
+                        if word[i] in k:
+                            remove_word = True
+            
+            # Remove the word if flagged
+            if remove_word:
+                try:
+                    words.remove(k)
+                except ValueError:
+                    continue
         
+        if words:
+            word = next_word()
+            print("this is the word!!!! ", word)
+            words.remove(word)
+        else:
+            print("No words left to guess.")
+            break
+    
+    return word
+
 play()
